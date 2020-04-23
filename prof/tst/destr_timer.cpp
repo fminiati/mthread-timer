d7899ea4b7f0a7c5a020d9b2d66ef8019572ab5b
francesco-miniati <francesco.miniati@gmail.com>
2020-04-23 17:46:32 +0000

    Update to new version of Timer
    - use new Timer_t<G,R,C> featuring granularity switch instead of Timer<R,C>

--- prof/tst/destr_timer.cpp
+++ prof/tst/destr_timer.cpp
@@ -1,4 +1,4 @@
-// Test destructor-call sequence. Specifically check that Timer<>'s destructor
+// Test destructor-call sequence. Specifically check that Timer_t<>'s destructor
 // gets called last within scope, as you would expect (from stack unwindind).
 //
 #include <iostream>
@@ -29,43 +29,43 @@ int main(int argc, char* argv[]) {
     using namespace std::chrono_literals;
     using namespace fm_profile;
 
-	Timer<> tmr("main");
+	Timer_t<> tmr("main");
 
-	{  Timer<> t{"hello"};
-	    std::cout << "Hello Timer!\n";
+	{  Timer_t<> t{"hello"};
+	    std::cout << "Hello Timer_t!\n";
 	    Foo foo("foo_hello");
 	    Bar bar("bar_hello");
 		std::this_thread::sleep_for(1ms);
-        { Timer<> t{"indent"}; std::this_thread::sleep_for(0.5ms);
+        { Timer_t<> t{"indent"}; std::this_thread::sleep_for(0.5ms);
 	    Bar bar("bar_indent");
 	    Foo foo("foo_indent after bar_indent");
-            { Timer<> t{"++dent"}; std::this_thread::sleep_for(1.5ms);
+            { Timer_t<> t{"++dent"}; std::this_thread::sleep_for(1.5ms);
 	    Bar bar("bar_++dent");
 	    Foo foo("foo_++dent after bar_++dent");
             }
-            { Timer<> t{"++bent"}; std::this_thread::sleep_for(0.3ms);
+            { Timer_t<> t{"++bent"}; std::this_thread::sleep_for(0.3ms);
 	    Foo foo("foo_++bent");
 	    Bar bar("bar_++bent after foo_++bent");
             }
-            { Timer<> t{"++bore"}; std::this_thread::sleep_for(0.2ms);
+            { Timer_t<> t{"++bore"}; std::this_thread::sleep_for(0.2ms);
 	    Foo foo("foo_++bore");
-                { Timer<> t{"innermost"}; std::this_thread::sleep_for(0.1ms);
+                { Timer_t<> t{"innermost"}; std::this_thread::sleep_for(0.1ms);
 	    Bar bar("bar_innermostr after foo_++bore");
                 }
             }
         }
-        { Timer<> t{"postdent"}; std::this_thread::sleep_for(1.2ms);
+        { Timer_t<> t{"postdent"}; std::this_thread::sleep_for(1.2ms);
 	  Foo* foobar=new Bar("foobar");
-          { Timer<> t{"inpost"}; std::this_thread::sleep_for(2ms);
+          { Timer_t<> t{"inpost"}; std::this_thread::sleep_for(2ms);
           }
 	  delete foobar;
         }
 	}
-    { Timer<> t{"posthello"}; //std::this_thread::sleep_for(1.1ms);
+    { Timer_t<> t{"posthello"}; //std::this_thread::sleep_for(1.1ms);
 	    Foo foo("foo_posthello");
 	    Bar bar("bar_ after foo_posthello");
-      { Timer<> t{"phindent"}; std::this_thread::sleep_for(1ms);}
-      { Timer<> t{"++phdent"}; std::this_thread::sleep_for(0.5ms);}
+      { Timer_t<> t{"phindent"}; std::this_thread::sleep_for(1ms);}
+      { Timer_t<> t{"++phdent"}; std::this_thread::sleep_for(0.5ms);}
     }
 
 	tmr.stop();
--- prof/tst/hello_timer.cpp
+++ prof/tst/hello_timer.cpp
@@ -8,30 +8,30 @@ int main(int argc, char* argv[]) {
     using namespace std::chrono_literals;
     using namespace fm_profile;
 
-	Timer<> tmr("main");
+	Timer_t<> tmr("main");
 
-	{  Timer<> t{"hello"};
-	   { Timer<> t("cout"); std::cout << "Hello Timer!\n";}
-           { Timer<> t{"indent"}; //std::this_thread::sleep_for(0.5ms);
-             { Timer<> t{"++dent"}; std::this_thread::sleep_for(1.5ms);
+	{  Timer_t<2> t{"hello"};
+	   { Timer_t<3> t("cout"); std::cout << "Hello Timer_t!\n";}
+           { Timer_t<3> t{"indent"}; //std::this_thread::sleep_for(0.5ms);
+             { Timer_t<4> t{"++dent"}; std::this_thread::sleep_for(1.5ms);
              }
-             { Timer<> t{"++bent"}; std::this_thread::sleep_for(0.3ms);
+             { Timer_t<4> t{"++bent"}; std::this_thread::sleep_for(0.3ms);
              }
-             { Timer<> t{"++bore"}; //std::this_thread::sleep_for(0.2ms);
-                { Timer<> t{"innermost"}; std::this_thread::sleep_for(0.1ms);
+             { Timer_t<4> t{"++bore"}; //std::this_thread::sleep_for(0.2ms);
+                { Timer_t<5> t{"innermost"}; std::this_thread::sleep_for(0.1ms);
                 }
              }
            }
-	   { Timer<> t{"postdent"}; //std::this_thread::sleep_for(1.2ms);
-             { Timer<> t{"inpost"}; std::this_thread::sleep_for(2ms);
+	   { Timer_t<3> t{"postdent"}; //std::this_thread::sleep_for(1.2ms);
+             { Timer_t<2> t{"inpost"}; std::this_thread::sleep_for(2ms);
              }
            }
 	}
-        { Timer<> t{"posthello"}; //std::this_thread::sleep_for(1.1ms);
-          { Timer<> t{"phindent"}; std::this_thread::sleep_for(1ms);}
-          { Timer<> t{"++phdent"}; std::this_thread::sleep_for(0.5ms);}
+        { Timer_t<2> t{"posthello"}; //std::this_thread::sleep_for(1.1ms);
+          { Timer_t<3> t{"phindent"}; std::this_thread::sleep_for(1ms);}
+          { Timer_t<3> t{"++phdent"}; std::this_thread::sleep_for(0.5ms);}
         }
 
 	tmr.stop();
-	Timer<>::print_record();
+	Timer_t<>::print_record();
 }
