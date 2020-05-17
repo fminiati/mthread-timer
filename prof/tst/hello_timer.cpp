fed137ffc978015070cd1c7f934b6d6411a815a0
francesco-miniati <francesco.miniati@gmail.com>
2020-05-17 11:34:41 +0000

    Implement loop of measurements
    - loop through timed function calls to make measurements more substantial
      and add a bit of statistics
    - read number of loops from user's commnad line input

--- prof/tst/hello_timer.cpp
+++ prof/tst/hello_timer.cpp
@@ -12,15 +12,26 @@ int main(int argc, char* argv[]) {
     using namespace std::chrono_literals;
     using namespace fm::profiling;
 
+    std::cout << "Hello Timer_t!\n";
     const std::string prog(argv[0]);
 
-#ifdef MULTI_THREAD
+    int n_loops{0};
+    for (auto i{0}; i<argc; ++i)
+        if (strncmp(argv[i],"-nl",3)==0) n_loops=std::stoi(argv[i+1]);
+
+#ifndef MULTI_THREAD
+    if (n_loops<=0) {
+        std::cout<<"\n number of loops="<<n_loops<<".\n Run this prog with: "+prog+" -nl num_loops \n\n";
+        return 0;
+    }
+#else
     int n_threads{0};
     for (auto i{0}; i<argc; ++i)
-        if (strncmp(argv[i],"-n",3)==0) n_threads=std::stoi(argv[i+1]);
+        if (strncmp(argv[i],"-nt",3)==0) n_threads=std::stoi(argv[i+1]);
 
-    if (n_threads<=0) {
-        std::cout<<"\nthread count="<<n_threads<<". Run this prog with: "+prog+" -n num_threds\n\n";
+    if (n_threads<=0 || n_loops<=0) {
+        std::cout << "\n thread count=" << n_threads << " and number of loops=" << n_loops << ".\n" 
+                  << " Run this prog with: "+prog+" -nt num_threads -nl num_loops \n\n";
         return 0;
     }
 
@@ -30,9 +41,9 @@ int main(int argc, char* argv[]) {
 	Timer_t<> tmr("main");
     std::string timer_prefix{};
 
-    auto timering=[s=timer_prefix]() {
+    auto timering=[&s=timer_prefix]() {
 	   Timer_t<2> t{s+"hello"};
-	   { Timer_t<3> t("cout"); std::cout << "Hello Timer_t!\n";}
+	   { Timer_t<3> t("cout");  }
            { Timer_t<3> t{"indent"}; //std::this_thread::sleep_for(0.5ms);
              { Timer_t<4> t{"++dent"}; std::this_thread::sleep_for(1.5ms);
              }
@@ -48,30 +59,34 @@ int main(int argc, char* argv[]) {
              }
            }
 	};
-    auto timering_more=[s=timer_prefix]() {
+    auto timering_more=[&s=timer_prefix]() {
         { Timer_t<2> t{s+"posthello"}; //std::this_thread::sleep_for(1.1ms);
           { Timer_t<3> t{"phindent"}; std::this_thread::sleep_for(1ms);}
           { Timer_t<3> t{"++phdent"}; std::this_thread::sleep_for(0.5ms);}
         }
     };
 
-    timering();
+    for (auto i{0}; i<n_loops; ++i) {
+
+        timering();
 
 #ifdef MULTI_THREAD
-    timer_prefix="main::";
-    std::vector<std::future<void>> fs;
-    for (auto i{1}; i<n_threads; ++i) fs.emplace_back(std::async(timering));
-    for (auto& f : fs) f.wait();
-    timer_prefix={};
+        timer_prefix="main::";
+        std::vector<std::future<void>> fs;
+        for (auto i{1}; i<n_threads; ++i) fs.emplace_back(std::async(timering));
+        for (auto& f : fs) f.wait();
+        timer_prefix={};
 #endif
 
-    timering_more();
+        timering_more();
 
 #ifdef MULTI_THREAD
-    timer_prefix="main::";
-    for (auto& f : fs) f=std::move(std::async(std::launch::async,timering_more));
-    for (auto& f : fs) f.wait();
+        timer_prefix="main::";
+        for (auto& f : fs) f=std::move(std::async(std::launch::async,timering_more));
+        for (auto& f : fs) f.wait();
+        timer_prefix={};
 #endif
+    }
 
 	tmr.stop();
 	Timer_t<>::print_record();
